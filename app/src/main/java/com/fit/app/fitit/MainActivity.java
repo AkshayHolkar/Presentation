4af837396b6e0714f20efdf59385b90a4d5fc37b
keysers0ze <kevinguemail@gmail.com>
2018-05-24 19:22:35 +1000

    - added code for a splash screen  - added splash screen .png file

--- app/src/main/java/com/fit/app/fitit/MainActivity.java
+++ app/src/main/java/com/fit/app/fitit/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Make sure this is before calling super.onCreate
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //find and set toolbar
