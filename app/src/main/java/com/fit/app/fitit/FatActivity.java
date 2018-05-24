ab19086990c63e927622c718e810807d62ac9c0e
keysers0ze <kevinguemail@gmail.com>
2018-05-24 22:56:20 +1000

    - fixed buttons

--- app/src/main/java/com/fit/app/fitit/FatActivity.java
+++ app/src/main/java/com/fit/app/fitit/FatActivity.java
@@ -25,7 +25,7 @@ public class FatActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fat);
 
         //find and set toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
